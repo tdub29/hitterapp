013bad5598e4a430e6dcc1f96518f4bc7225626b
tdub29 <108891102+tdub29@users.noreply.github.com>
2024-12-30 13:42:56 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -422,8 +422,7 @@ def plot_pitch_locations_by_playresult(data):
     # Add a single colorbar legend for xSLG across all plots
     sm = plt.cm.ScalarMappable(cmap='coolwarm', norm=plt.Normalize(vmin=0, vmax=2))
     sm.set_array([])  # Required for ScalarMappable
-    
-    cbar_ax = fig.add_axes([0.3, 0.07, 0.4, 0.02])  # Define position for colorbar
+    cbar_ax = fig.add_axes([0.92, 0.3, 0.02, 0.4])  # x, y, width, height (adjust for positioning)
     cbar = fig.colorbar(sm, cax=cbar_ax, orientation='vertical')
     cbar.set_label('xSLG (0 - 2)', fontsize=12, color='white')
     cbar.ax.tick_params(labelcolor='darkblue')
